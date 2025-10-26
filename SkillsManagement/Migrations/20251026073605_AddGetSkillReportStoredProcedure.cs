a6f9d14287cb1aed62b03478746b80d3a7fd3a4b
Rushikesh Gunjal <rushigunjal2016@gmail.com>
2025-10-26 13:09:41 +0530

    Added SP in the migrations so that user will need to manually add it in database

---
+++ SkillsManagement/Migrations/20251026073605_AddGetSkillReportStoredProcedure.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillsManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetSkillReportStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS GetSkillReport");
+            
+            var sp = @"
+CREATE PROCEDURE GetSkillReport
+    @CustomerId INT
+AS
+BEGIN
+    SELECT
+        SkillName,
+        COUNT(DISTINCT EmployeeId) AS NoOfEmployees,
+        MIN(Rating) AS MinRating,
+        MAX(Rating) AS MaxRating,
+        AVG(CAST(Rating AS FLOAT)) AS AvgRating
+    FROM
+        EmployeeSkills
+    WHERE
+        CustomerId = @CustomerId
+    GROUP BY
+        SkillName
+END";
+            migrationBuilder.Sql(sp);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS GetSkillReport");
+        }
+    }
+}
